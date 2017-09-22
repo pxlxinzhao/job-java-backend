23c60549c71b61fb307dd9fc3cabd099f912e05e
patrickpu <patrickpu@NB-PATRICK.FORTIGO.FORTIGOFREIGHT.COM>
2017-09-22 11:29:10 -0400

    add post

--- src/main/java/com/patrickpu/job/config/AppConfig.java
+++ src/main/java/com/patrickpu/job/config/AppConfig.java
@@ -1,5 +1,17 @@
 package com.patrickpu.job.config;
 
+import static org.hibernate.cfg.AvailableSettings.C3P0_ACQUIRE_INCREMENT;
+import static org.hibernate.cfg.AvailableSettings.C3P0_MAX_SIZE;
+import static org.hibernate.cfg.AvailableSettings.C3P0_MAX_STATEMENTS;
+import static org.hibernate.cfg.AvailableSettings.C3P0_MIN_SIZE;
+import static org.hibernate.cfg.AvailableSettings.C3P0_TIMEOUT;
+import static org.hibernate.cfg.AvailableSettings.DRIVER;
+import static org.hibernate.cfg.AvailableSettings.HBM2DDL_AUTO;
+import static org.hibernate.cfg.AvailableSettings.PASS;
+import static org.hibernate.cfg.AvailableSettings.SHOW_SQL;
+import static org.hibernate.cfg.AvailableSettings.URL;
+import static org.hibernate.cfg.AvailableSettings.USER;
+
 import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +25,8 @@ import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.patrickpu.job.model.Post;
 import com.patrickpu.job.model.User;
-
-import static org.hibernate.cfg.Environment.*;
 
 /**
  * @author imssbora
@@ -59,6 +70,7 @@ public class AppConfig {
 
       factoryBean.setHibernateProperties(props);
       factoryBean.setAnnotatedClasses(User.class);
+      factoryBean.setAnnotatedClasses(Post.class);
       return factoryBean;
    }
 
