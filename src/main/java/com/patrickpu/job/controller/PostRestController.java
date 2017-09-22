8e42e6e48ecb2d70b373d2683d213909aa13b9f1
Patrick Pu <patrickpu@FORTIGO.FORTIGOFREIGHT.COM>
2017-09-22 14:34:22 -0400

    “CORS”

--- src/main/java/com/patrickpu/job/controller/PostRestController.java
+++ src/main/java/com/patrickpu/job/controller/PostRestController.java
@@ -3,6 +3,7 @@ package com.patrickpu.job.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class PostRestController {
 	   private PostService postService;
 
 	   @RequestMapping(method = RequestMethod.GET, value = "/list")
+	   @CrossOrigin(origins = "http://localhost:8100")
 	   public List<Post> listAllPost() {
 		  List<Post> posts = postService.list();
 	      return posts;
